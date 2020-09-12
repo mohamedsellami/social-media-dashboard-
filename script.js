8462017f86d4cbceb8764b7c48d8cc5001bf09df
Mohamed-Sellami <64484505+mohamedsellami@users.noreply.github.com>
2020-09-12 14:40:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const switcher = document.querySelector(".switch button");
 const body = document.querySelector("body");
 switcher.addEventListener("click", switchBetweenDarkAndLightTheme);
 function switchBetweenDarkAndLightTheme() {
-  if (circle.style.transform == "translateX(20px)") {
-    circle.style.transform = "translateX(0px)";
+  if (circle.style.transform == "translateX(0px)") {
+    circle.style.transform = "translateX(20px)";
+    circle.style.backgroundColor = "white";
     body.classList.remove("darkTheme");
   } else {
-    circle.style.transform = "translateX(20px)";
+    circle.style.transform = "translateX(0px)";
+    circle.style.backgroundColor = "hsl(232, 19%, 15%)";
     body.classList.add("darkTheme");
   }
 }
